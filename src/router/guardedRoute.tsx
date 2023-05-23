2b5defcf4b3a6174175694b248f12e06d9f3275e
Yensy Gonzalez <yensy@yensysmacstudio.lan>
2023-05-23 11:34:59 -0500

    solved login redirect issue

--- src/router/guardedRoute.tsx
+++ src/router/guardedRoute.tsx
@@ -1,10 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import useUser from 'hooks/useUser';
 import { Navigate, Outlet } from 'react-router-dom';
+import { routePaths } from 'utils/constants';
 // eslint-disable-next-line
 import { guardedRouteTypes } from 'utils/proptypes';
 
 const GuardedRoute = ({ path, isNestedRoute = false, nestedRouteRedirectUrl }: guardedRouteTypes) => {
+  const {isLoading, isAuthenticated} = useAuth0();
+
   const { userPermissionProfile } = useUser();
+
+  if(!isLoading && !isAuthenticated){
+    return <Navigate to={routePaths.LOGIN_PAGE} />
+  }
 
   const userIsAuthorized = userPermissionProfile?.permittedRoutes.find(
     route => route === path,
